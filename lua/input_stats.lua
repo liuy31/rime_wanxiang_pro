b71a446b83005b0ad9a5c5537c28fe2e7eb26862
amzxyz <rcm@qq.com>
2025-06-15 23:20:54 +0800

    更新

---
+++ lua/input_stats.lua
@@ -0,0 +1,31 @@
+input_stats = {
+    ["yearly"] = {
+    ["fastest"] = 0,
+    ["ts"] = 0,
+    ["count"] = 0,
+    ["length"] = 0,
+},
+    ["recent"] = {
+},
+    ["daily"] = {
+    ["fastest"] = 0,
+    ["ts"] = 0,
+    ["count"] = 0,
+    ["length"] = 0,
+},
+    ["daily_max"] = 0,
+    ["lengths"] = {
+},
+    ["monthly"] = {
+    ["fastest"] = 0,
+    ["ts"] = 0,
+    ["count"] = 0,
+    ["length"] = 0,
+},
+    ["weekly"] = {
+    ["fastest"] = 0,
+    ["ts"] = 0,
+    ["count"] = 0,
+    ["length"] = 0,
+},
+}
